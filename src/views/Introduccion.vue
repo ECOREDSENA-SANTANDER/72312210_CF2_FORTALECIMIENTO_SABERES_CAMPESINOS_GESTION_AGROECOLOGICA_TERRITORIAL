63bd69308ba15fb8b4d6d9d93d58dfcd505d9232
lizmanchego <karinamanchego0416@gmail.com>
2026-09-07 17:26:48 -0500

    ADD Video + Podcast / UPD code CF

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -34,7 +34,9 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/p80eJToRwZA?si=GaTPx-DcYIfJPY6K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          figcaption Video. Caracterización del sistema productivo campesino y diseño de planes de mejora
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -304,7 +304,7 @@
   </template>
 
 <script>
-import podcast from '@/assets/actividad/audio/success.mp3'
+import podcast from '@/assets/curso/temas/t3/podcast.mp3'
 export default {
   name: 'Tema3',
   data: () => ({
